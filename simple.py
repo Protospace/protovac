1cbb32779b73506186e60335ced31b295ddc8c58
Tanner Collin <git@tannercollin.com>
2022-09-01 01:33:26 -0600

    Add simple curses example

---
+++ simple.py
@@ -0,0 +1,19 @@
+#!/home/pi/protovac/env/bin/python
+
+import time
+import curses
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(True)
+
+stdscr.addstr(5, 5, 'Hello world!')
+stdscr.refresh()
+
+stdscr.getch()
+
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+curses.endwin()
